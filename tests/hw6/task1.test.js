0350b464eacbdd3f6ef10c4f92cf99090a4fbeac
Anna Plotitsyna <tuaeva_anna@mail.ru>
2022-05-10 19:10:25 +0300

    hw6 task1 done

---
+++ tests/hw6/task1.test.js
@@ -0,0 +1,21 @@
+const task1 = require('../../tasks/hw6/task1');
+const getDiff = task1.getDiff;
+
+
+describe("getDiff", () => {
+    it('is a function', () => {
+        expect(typeof getDiff).toBe("function");
+    });
+    it('Function getDiff return diff between 10 and 2 = 8', () => {
+        expect(getDiff(10, 2)).toBe(8);
+    });
+    it('Function getDiff return diff between -2 and -3 = 1', () => {
+        expect(getDiff(-3, -2)).toBe(1);
+    }); 
+    it('Function getDiff return diff between 0 and -3 = 3', () => {
+        expect(getDiff(0, -3)).toBe(3);
+    });        
+    it('function"s return is a typeof "number"', () => {
+        expect(typeof getDiff(0, -3)).toBe("number");
+    });        
+});
