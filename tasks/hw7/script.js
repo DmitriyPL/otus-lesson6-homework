b5e9fa510c6998bdae22157cc06f5ae83cffcc9c
Dmitriy Plotitsyn <plhome@yandex.ru>
2022-05-11 20:52:53 +0300

    refactor task

--- tasks/hw7/script.js
+++ tasks/hw7/script.js
@@ -1,40 +1,40 @@
-(async function () {
+module.exports = { saveList };
+// , readList, drawList, submitHandler
 
-    function saveList(items) {
-        localStorage.setItem("list", JSON.stringify(items));
+function saveList(key, items) {
+    localStorage.setItem(key, JSON.stringify(items));
+}
+
+function readList() {
+    let items = JSON.parse(localStorage.getItem("list"));
+    return items === null ? [] : items;
+}
+
+function drawList(el, items) {
+    el.innerHTML = `<ol>${items.map((el) => `<li>${el}</li>`).join("")}</ol>`;
+}
+
+function submitHandler(event, items){
+    const formEl = event.target;
+    const input = formEl.querySelector("input");
+    const value = input.value;
+    input.value = "";
+
+    items.push(value);
+
+    if (items.length > 5){
+        items.shift();
     }
+}
 
-    async function readList() {
-        let items = JSON.parse(localStorage.getItem("list"));
-        return items === null ? [] : items;
-    }
+const form = document.querySelector("form");
+const listEl = document.querySelector("#list");
+const items = readList();
 
-    function drawList(el, items) {
-        el.innerHTML = `<ol>${items.map((el) => `<li>${el}</li>`).join("")}</ol>`;
-    }
+drawList(listEl, items);
 
-    const form = document.querySelector("form");
-    const listEl = document.querySelector("#list");
-    const items = await readList();
-
+form.addEventListener("submit", (event) => {
+    submitHandler(event, items)
     drawList(listEl, items);
-
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        const formEl = event.target;
-        const input = formEl.querySelector("input");
-        const value = input.value;
-        input.value = "";
-
-        items.push(value);
-
-        if (items.length > 5){
-            items.shift();
-        }
-
-        drawList(listEl, items);
-        saveList(items);
-
-    });
-})();
+    saveList("list", items);
+});
